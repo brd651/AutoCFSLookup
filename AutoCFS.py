41b361346b10fe7aae5cd54eeb6bfee649d74acc
brd651 <brd651@gmail.com>
2018-03-11 12:03:59 -0700

    Update AutoCFS.py

--- AutoCFS.py
+++ AutoCFS.py
@@ -1,15 +1,17 @@
 import time
+import urllib2
+import csv
+import ctypes
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import csv
 
-with open("C:\Users\\<USER>\Desktop\\test.csv", "rb") as data:
+with open("test.csv", "rb") as data:
     reader = csv.DictReader(data, delimiter=',')
     Domain = []
     for row in reader:
         Domain.append(row['Domain'])
 
-headers = "Domain,CategoryID1,CategoryName1,CategoryID2,CategoryName2,CategoryID3,CategoryName3,CategoryID4,CategoryName4"
+headers = "Domain,HTTP,HTTPS,CategoryID1,CategoryName1,CategoryID2,CategoryName2,CategoryID3,CategoryName3,CategoryID4,CategoryName4"
 
 try:
     b = open('file1.csv')
@@ -19,14 +21,15 @@ except IOError:
     createfile.write(headers)
     createfile.close()
 
-driver=webdriver.Edge("C:\\Users\\<USER>\\Downloads\\MicrosoftWebDriver.exe")
+driver = webdriver.Edge("C:\\Users\\brd65\\Downloads\\MicrosoftWebDriver.exe")
 
 driver.set_page_load_timeout(30)
 driver.get("http://cfssupport.sonicwall.com")
 driver.implicitly_wait(20)
 driver.find_element_by_id("url").send_keys("vk.com")
 driver.find_element_by_name("kaptcha").click()
-time.sleep (10)
+MessageBox = ctypes.windll.user32.MessageBoxA
+MessageBox(None, 'Click OK after Completing Kaptcha', 'Attention', 0)
 driver.find_element_by_name("button").click()
 
 for k in driver.find_elements_by_xpath('/html/body/div/form/div[3]/div/table/tbody'):
@@ -43,7 +46,7 @@ for a in Domain:
     driver.find_element_by_id("url").send_keys(Keys.BACKSPACE)
     driver.find_element_by_id("url").send_keys(a)
     driver.find_element_by_name("button").click()
-    time.sleep(0.25)
+    time.sleep(1)
     for i in driver.find_elements_by_xpath('/html/body/div/form/div[3]/div/table/tbody'):
         i2 = i.get_attribute('innerHTML')
         i3 = "".join(i2.split())
@@ -51,6 +54,5 @@ for a in Domain:
         i5 = i4.replace("</b><br></td></tr>", "").replace("Category", ":").replace(":", ",")
         update = open("file1.csv", "a")
         update.write(a + i5 + ",\n")
-    time.sleep(0.25)
 
 driver.quit()
